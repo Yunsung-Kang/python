d6ed65a4c96ff6d48f9f2317a0d67a3f44e88543
사용자이름 <ysst0511@gmail.com>
2024-03-18 15:56:04 +0900

    feat : pandas rngus - head,tail - describe - DataFrame - mode - sort - fillna - boxplot

---
+++ Pd_base.py
@@ -0,0 +1,32 @@
+#packages load
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+d= {"one" : range(20), "two" : np.random.randn(20)}
+df= pd.DataFrame(d)
+
+df.head()
+df.tail(1)
+
+df.shape	#data size
+df.describe()	#data summary
+
+df2=pd.DataFrame({'first':[2,2,2,2,4,4,6,6,6,6,6,6,10],
+                  'second':['one','one','one','two','two','three','three','three','three','three','four','four','four']})
+df2.value_counts()
+df2.mode()
+
+df.sort_values(by='two')	#오름차순
+df.sort_values(by='two',ascending=False)	#내림차순
+df['two'].sort_values()
+
+# fillna() = null 값을 채워줌.
+
+df3= pd.DataFrame({
+    'unif' : np.random.uniform(-3,3,20),
+    'norm' : np.random.normal(0,1,20)
+})
+
+# 시각화
+plt.boxplot(df)
+plt.show()
